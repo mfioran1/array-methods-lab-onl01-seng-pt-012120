a82b2b1debdcae2195d5317a166804b0d5797f01
Maurice Fiorani <mauricef97@gmail.com>
2020-01-29 23:38:10 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,6 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
- array.include?("tardis")
+ array.include?("wow", "tardis")
 end
 
 def using_sort(array)
